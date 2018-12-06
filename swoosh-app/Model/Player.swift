48152caac819228e79f20d9385201477eaf9ab82
CodingCherry <cherry.alexeeva@gmail.com>
2018-12-06 16:26:00 +0700

    Data model added

---
+++ swoosh-app/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  swoosh-app
+//
+//  Created by Alexeeva Ekaterina on 12/6/18.
+//  Copyright © 2018 Alexeeva Ekaterina. All rights reserved.
+//
+
+import Foundation
+
+struct  Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
